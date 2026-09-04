b055417a735cce7af6c4b04ebcfae599c54122ce
maceves3-png <Maceves3@ucol.mx>
2026-09-04 09:23:53 -0600

    ultima vercion

--- main.c
+++ main.c
@@ -4,7 +4,7 @@ int main(void) {
     printf("===============================\n");
     printf("         ¡Hola, GITHUB!\n");
     printf("==============================\n\n");
-    printf("Mi nombre es: Jorge anddres\n");
+    printf("Mi nombre es: Jorge andres\n");
     printf("Estoy aprendiendo programacion.\n");
     printf("Este es mi primer programa\n");
     printf("Publicado en Github\n\n");
